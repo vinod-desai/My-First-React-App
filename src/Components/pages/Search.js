0278fbef54ac9b5a2eb45d26ab08a6f8e04d0f18
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2019-04-06 10:58:19 +0530

    Update Search.js

--- src/Components/pages/Search.js
+++ src/Components/pages/Search.js
@@ -11,8 +11,8 @@ export class Search extends Component {
     onSubmit = (e) => {
         e.preventDefault();
         //this.props.addTodo(this.state.srchTxt);
-        // axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=c32275c1')
-       let url = `http://www.omdbapi.com/?s=${this.state.srchTxt}&apikey=c32275c1`;
+        // axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=a12275b1')
+       let url = `http://www.omdbapi.com/?s=${this.state.srchTxt}&apikey=a12275b1`;
         axios.get(url)
         .then(res => this.setState( { results: res.data.Search }));
 
